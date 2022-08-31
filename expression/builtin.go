980f26a1ef6f1f8da8136925387753037c70c0f5
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-08-31 17:47:44 +0530

    Update builtin.go

--- expression/builtin.go
+++ expression/builtin.go
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSES/QL-LICENSE file. 
 
 // Copyright 2015 PingCAP, Inc.
-//
+// 
 // Licensed under the Apache License, Version 2.0 (the "License");  
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
